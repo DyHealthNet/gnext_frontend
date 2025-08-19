b11d39a852aa565ebad1dd5c74bfba34d64bc031
bastiennerehor <bastienne.rehor@tum.de>
2025-08-19 12:51:17 +0200

    semicolon was missing

--- src/components/trait/AutocompleteVariant.vue
+++ src/components/trait/AutocompleteVariant.vue
@@ -53,28 +53,29 @@ export default {
     };
   },
   created() {
-      const searchParams = ref({
-        query_by: "description,external_ref,category",
-        num_typos: 0,
-        drop_tokens_threshold: 1.0,
-      });
-      this.typesense = new TypesenseInstantSearchAdapter({
-        server: {
-          apiKey: "xyz",
-          nodes: [{ host: TYPESENSE_HOST, port: TYPESENSE_PORT, protocol: "http", path: "" }],
-          cacheSearchResultsForSeconds: 120,
-        },
-        additionalSearchParameters: searchParams.value,
-      });
-      this.searchClient = this.typesense.searchClient
-    methods: {
-      selectRsid(hit, refine) {
-        if (hit.type === "variant") {
-          // populate the rsid field with external_ref (or hit.id)
-          this.$emit('update-variant', { rsid: hit.external_ref, varid: hit.id });
-          refine(''); // clear the search box suggestions
-        }
+    const searchParams = ref({
+      query_by: "description,external_ref,category",
+      num_typos: 0,
+      drop_tokens_threshold: 1.0,
+    });
+    this.typesense = new TypesenseInstantSearchAdapter({
+      server: {
+        apiKey: "xyz",
+        nodes: [{host: TYPESENSE_HOST, port: TYPESENSE_PORT, protocol: "http", path: ""}],
+        cacheSearchResultsForSeconds: 120,
       },
+      additionalSearchParameters: searchParams.value,
+    });
+    this.searchClient = this.typesense.searchClient
+  },
+  methods: {
+    selectRsid(hit, refine) {
+      if (hit.type === "variant") {
+        // populate the rsid field with external_ref (or hit.id)
+        this.$emit('update-variant', { rsid: hit.external_ref, varid: hit.id });
+        refine(''); // clear the search box suggestions
+      }
     },
-  };
+  },
+};
 </script>
